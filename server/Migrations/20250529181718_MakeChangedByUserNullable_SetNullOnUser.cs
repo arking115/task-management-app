dab0c6b629ddac2bddcabc632b5ae1173ed6b3a4
Paul <vilau.iu.paul@student.utcluj.ro>
2025-05-29 21:44:57 +0300

    ChangedBy_changes

---
+++ server/Migrations/20250529181718_MakeChangedByUserNullable_SetNullOnUser.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeChangedByUserNullable_SetNullOnUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskHistories_Users_ChangedByUserId",
+                table: "TaskHistories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChangedByUserId",
+                table: "TaskHistories",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskHistories_Users_ChangedByUserId",
+                table: "TaskHistories",
+                column: "ChangedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskHistories_Users_ChangedByUserId",
+                table: "TaskHistories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChangedByUserId",
+                table: "TaskHistories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskHistories_Users_ChangedByUserId",
+                table: "TaskHistories",
+                column: "ChangedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
